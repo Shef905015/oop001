1876eec54566b435f79aaf5aea87220736a6dcdc
Non't <90344104+Shef905015@users.noreply.github.com>
2023-11-29 09:13:16 +0100

    Add files via upload

---
+++ oop001ab/Osoba.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop001ab
+{
+    internal class Osoba
+    {
+
+        string ime, prezime;
+        int godinaRodjenja, visina, tezina;
+        char spol;
+
+        public void setIme(string ime)
+        {
+            this.ime = ime;
+        }
+        public void setPrezime(string prezime)
+        {
+            this.prezime = prezime;
+        }
+        public void setGodinaRodjenja(int godinaRodjenja)
+        {
+            this.godinaRodjenja = godinaRodjenja;
+        }
+        public void setVisina(int visina)
+        {
+            this.visina = visina;
+        }
+        public void setTezina(int tezina)
+        {
+            this.tezina = tezina;
+        }
+        public void setSpol(char spol)
+        {
+            this.spol = spol;
+        }
+        //-----------------------------------------------
+        public string getIme()
+        {
+            return this.ime;
+        }
+        public string getPrezime()
+        {
+            return this.prezime;
+        }
+        public int getGodinaRodjenja()
+        {
+            return this.godinaRodjenja;
+        }
+        public int getVisina()
+        {
+            return this.visina;
+        }
+        public int getTezina()
+        {
+            return this.tezina;
+        }
+        public char getSpol()
+        {
+            return this.spol;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Ime i prezime:  " + this.getIme() + " " + this.getPrezime() + "\n"
+            + "Godina rođenja: " + this.getGodinaRodjenja() + "\n"
+            + "Visina: " + this.getVisina() + " cm\n"
+            + "Težina " + this.getTezina() + " kg\n"
+            + "Spol: " + this.getSpol() + "\n";
+            return ispis;
+        }
+
+        public Osoba(String ime, String prezime, int godinaRodjenja, int visina, int tezina, char spol)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.godinaRodjenja = godinaRodjenja;
+            this.visina = visina;
+            this.tezina = tezina;
+            this.spol = spol;
+        }
+
+        public Osoba() { }
+    }
+}
---
+++ oop001ab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop001ab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba = new Osoba();
+
+            osoba.ToString();
+
+            osoba.setIme("Pero");
+            osoba.setPrezime("Pikunelić");
+            osoba.setVisina(169);
+            osoba.setTezina(80);
+            osoba.setGodinaRodjenja(1941);
+            osoba.setSpol('M');
+
+            Console.Write(osoba.ToString());
+
+            Osoba osoba2 = new Osoba("Vladimir", "Nazor", 168, 85, 1895, 'M');
+
+            Console.WriteLine(osoba2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
